ad72ff5ae0353c05c3ae44837621fe7aec6094a3
最甜宝宝 <htk90uggk@protonmail.com>
2019-09-03 21:19:43 +0800

    修复之前训练集与测试集为同一个数据集的问题，精度只有0.02%，等于没有训练

--- src/HCCC.py
+++ src/HCCC.py
@@ -13,19 +13,24 @@ from matplotlib import pyplot as plt
 from keras.utils import to_categorical
 
 # step = 1246991  # 全局变量，每次处理step个图片，防止占用太多内存，可以根据实际情况更改
-step = 3728  # 全局变量，每次处理step个图片，防止占用太多内存，可以根据实际情况更改
+step = 10000  # 全局变量，每次处理step个图片，防止占用太多内存，可以根据实际情况更改
 threshold = 220  # 二值图阈值
 TargetSize = 64  # 目标图片的边长
 times = 100  # 迭代次数
 
 # 从数据集中提取部分样本
-def GetPictures(gnt, imgs, labels):
+def GetPictures(gnt, imgs, labels, imgs_test, labels_test):
     i = 0
+    step2 = step * 2
     for img, label in gnt:
-        if i == step:
+        if i < step:
+            imgs[i] = img
+            labels[i] = label
+        elif i < step2:
+            imgs_test[i - step] = img
+            labels_test[i - step] = label
+        else:
             break
-        imgs[i] = img
-        labels[i] = label
         i = i + 1
 
 # 将灰度图转为二值图    
@@ -59,8 +64,7 @@ labels = [0 for x in range(0, step)]
 labels_str = []
 imgs_test = [0 for x in range(0, step)]
 labels_test = [0 for x in range(0, step)]
-GetPictures(gnt, imgs, labels)  # 获取数据集中的step个训练数据
-GetPictures(gnt, imgs_test, labels_test)  # 获取数据集中的step个测试数据
+GetPictures(gnt, imgs, labels, imgs_test, labels_test)  # 获取数据集中的step个训练数据
 table = []
 for i in range(256):
     if i < threshold:
@@ -71,7 +75,7 @@ for i in range(256):
 # 训练数据集
 for i in range(0, step):  # 统一图片大小
     imgs[i] = Image.fromarray(imgs[i])
-    imgs[i] = Gray2binary(table, imgs[i])  # 将灰度图转为二值图
+    #imgs[i] = Gray2binary(table, imgs[i])  # 将灰度图转为二值图
     imgs[i] = imgs[i].resize((TargetSize, TargetSize))
     imgs[i] = np.array(imgs[i])
     StrL2IntL(labels, labels_str)  # 处理中文标签
@@ -93,16 +97,18 @@ imgs_test = imgs_test.reshape((step, TargetSize * TargetSize))
 # 构建网络
 network = models.Sequential()
 network.add(layers.Dense(512, activation = 'relu', input_shape = (TargetSize * TargetSize, )))
-network.add(layers.Dense(step, activation='softmax'))
+network.add(layers.Dense(len(labels_str), activation='softmax'))
 
 # 编译
 network.compile(optimizer = 'rmsprop', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
 # 训练
+labels = np.array(labels)
 labels = to_categorical(labels)
 network.fit(imgs, labels, epochs=times, batch_size=128)
 
 # 测试
+labels_test = np.array(labels_test)
 labels_test = to_categorical(labels_test)
 test_loss, test_acc = network.evaluate(imgs_test, labels_test)
 print('test_acc:', test_acc)
